6905ccc74b3793ce8169bc9d92116b9b0876625f
Leilani Leach <leilanileach@yahoo.com>
2019-01-25 14:57:06 -0800

    add ngFor loop to display all top stories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FeaturedArticle } from '../app/featured-articles/models/FeaturedArticle';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nytimes';
+
+  topStory = new FeaturedArticle("Trump backs down over shutdown", "government shutdown", "lorem ipsum ipsum", "lorem lorem ipsum ipsum");
+  otherStory = new FeaturedArticle("Bad stuff happening in Venezuela", "Venezuela", "lorem ipsum ipsum", "lorem lorem ipsum ipsum");
+
+  featuredStories: FeaturedArticle[] = [
+    this.topStory, this.otherStory
+  ]
 }
