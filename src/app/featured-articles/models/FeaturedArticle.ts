781fb97881c32c68b8fdb1266b839ac70b1fa8ee
Leilani Leach <leilanileach@yahoo.com>
2019-01-25 14:08:07 -0800

    add FeaturedArticle class and constructor

---
+++ src/app/featured-articles/models/FeaturedArticle.ts
@@ -0,0 +1,7 @@
+export class FeaturedArticle {
+  bodyText: string;
+  picture: string;
+  whenPublished: Date;
+
+  constructor(public headline: string, public topic: string, public mainpoint1: string, public mainpoint2: string) { }
+}
