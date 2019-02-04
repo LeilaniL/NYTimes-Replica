9738aa915ae7e6526f2236d9029f514b5924ae2f
Leilani Leach <leilanileach@yahoo.com>
2019-02-03 18:08:00 -0800

    fix routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,14 +1,13 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
-import { FeaturedArticle } from '../app/featured-articles/models/FeaturedArticle';
 import { FeaturedArticleComponent } from '../app/featured-articles/featured-articles.component';
 import { ReadArticleComponent } from '../app/read-article/read-article.component';
 
 const appRoutes: Routes = [
-  // {
-  //     path: '',
-  //     component: HomepageComponent
-  // },
+  {
+    path: '',
+    component: FeaturedArticleComponent
+  },
   {
     path: 'read-article',
     component: ReadArticleComponent
--- src/app/featured-articles/featured-articles.component.ts
+++ src/app/featured-articles/featured-articles.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'featured-articles',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeaturedArticleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
