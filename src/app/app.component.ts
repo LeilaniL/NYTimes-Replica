f5e747309605ad240a19979a978ea5e1cc36ecc9
Leilani Leach <leilanileach@yahoo.com>
2019-02-03 22:35:02 -0800

    set up read ability for database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FeaturedArticle } from '../app/featured-articles/models/FeaturedArticle';
+import { FirebaseListObservable, AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +10,15 @@ import { FeaturedArticle } from '../app/featured-articles/models/FeaturedArticle
 })
 export class AppComponent {
   title = 'nytimes';
+  articles: FirebaseListObservable<FeaturedArticle[]>;
 
-  constructor(private router: Router) { };
+  constructor(private router: Router, private db: AngularFireDatabase) {
+    this.articles = db.list('/articles');
+  };
 
   topStory = new FeaturedArticle("Trump backs down over shutdown", "government shutdown", "lorem ipsum ipsum", "lorem lorem ipsum ipsum");
   otherStory = new FeaturedArticle("Bad stuff happening in Venezuela", "Venezuela", "lorem ipsum ipsum", "lorem lorem ipsum ipsum");
 
-  featuredStories: FeaturedArticle[] = [
-    this.topStory, this.otherStory
+  featuredStories: any[] = [
   ]
 } 
