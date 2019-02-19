32260c52c38301ace6356bd38608f7a9d5229daa
Leilani Leach <leilanileach@yahoo.com>
2019-02-19 06:28:53 -0800

    add article text to detail view

--- src/app/featured-articles/models/FeaturedArticle.ts
+++ src/app/featured-articles/models/FeaturedArticle.ts
@@ -3,5 +3,5 @@ export class FeaturedArticle {
   picture: string;
   whenPublished: Date;
 
-  constructor(public headline: string, public topic: string, public mainpoint1: string, public mainpoint2: string) { }
-}
+  constructor(public headline: string, public topic: string, public mainpoint1: string, public mainpoint2: string, public body: string) { }
+} 
